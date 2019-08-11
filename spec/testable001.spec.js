e3400f6b95db08e38e2b3b5aed40cb1a9dcf22eb
Bob Fornal <bob.fornal@leadingedje.com>
2019-08-11 10:29:19 -0400

    adjustment

--- spec/testable001.spec.js
+++ spec/testable001.spec.js
@@ -25,68 +25,70 @@ describe('Machining Better Tests', function() {
     testableCode.items = [];
   });
 
-  it('expects "Empty to Full: using push 10 times (10 objects)"', function() {
-    var push = 10;
-    for (var i = 0, len = push; i < len; i++) {
-      testableCode.push(i);
-    }
-    expect(testableCode.items.length).toEqual(10);
-  });
-
-  it('expects "Empty to Loaded: using push 4 times (4 objects)"', function() {
-    var push = 4;
-    for (var i = 0, len = push; i < len; i++) {
-      testableCode.push(i);
-    }
-    expect(testableCode.items.length).toEqual(4);
-  });
-
-  it('expects "Full to Empty: using pop 10 times (0 objects)"', function() {
-    testableCode.items = [1,2,3,4,5,6,7,8,9,10];
-    var pop = 10;
-    for (var i = 0, len = pop; i < len; i++) {
-      testableCode.pop();
-    }
-    expect(testableCode.items.length).toEqual(0);
-  });
-
-  it('expects "Full to Empty: using clear 1 time (0 objects)"', function() {
-    testableCode.items = [1,2,3,4,5,6,7,8,9,10];
-    testableCode.clear();
-    expect(testableCode.items.length).toEqual(0);
-  });
-
-  it('expects "Full to Loaded: using pop 6 times (4 objects)"', function() {
-    testableCode.items = [1,2,3,4,5,6,7,8,9,10];
-    var pop = 6;
-    for (var i = 0, len = pop; i < len; i++) {
-      testableCode.pop();
-    }
-    expect(testableCode.items.length).toEqual(4);
-  });
-
-  it('expects "Loaded to Empty: using pop 4 times (0 objects)"', function() {
-    testableCode.items = [1,2,3,4];
-    var pop = 4;
-    for (var i = 0, len = pop; i < len; i++) {
-      testableCode.pop();
-    }
-    expect(testableCode.items.length).toEqual(0);
-  });
-
-  it('expects "Loaded to Empty: using clear 1 time (0 objects)"', function() {
-    testableCode.items = [1,2,3,4];
-    testableCode.clear();
-    expect(testableCode.items.length).toEqual(0);
-  });
-
-  it('expects "Loaded to Full: using push 6 times (10 objects)"', function() {
-    testableCode.items = [1,2,3,4];
-    var push = 6;
-    for (var i = 0, len = push; i < len; i++) {
-      testableCode.push(i);
-    }
-    expect(testableCode.items.length).toEqual(10);
+  describe('Standard State Transitions', function() {
+    it('expects "Empty to Full: using push 10 times (10 objects)"', function() {
+      var push = 10;
+      for (var i = 0, len = push; i < len; i++) {
+        testableCode.push(i);
+      }
+      expect(testableCode.items.length).toEqual(10);
+    });
+  
+    it('expects "Empty to Loaded: using push 4 times (4 objects)"', function() {
+      var push = 4;
+      for (var i = 0, len = push; i < len; i++) {
+        testableCode.push(i);
+      }
+      expect(testableCode.items.length).toEqual(4);
+    });
+  
+    it('expects "Full to Empty: using pop 10 times (0 objects)"', function() {
+      testableCode.items = [1,2,3,4,5,6,7,8,9,10];
+      var pop = 10;
+      for (var i = 0, len = pop; i < len; i++) {
+        testableCode.pop();
+      }
+      expect(testableCode.items.length).toEqual(0);
+    });
+  
+    it('expects "Full to Empty: using clear 1 time (0 objects)"', function() {
+      testableCode.items = [1,2,3,4,5,6,7,8,9,10];
+      testableCode.clear();
+      expect(testableCode.items.length).toEqual(0);
+    });
+  
+    it('expects "Full to Loaded: using pop 6 times (4 objects)"', function() {
+      testableCode.items = [1,2,3,4,5,6,7,8,9,10];
+      var pop = 6;
+      for (var i = 0, len = pop; i < len; i++) {
+        testableCode.pop();
+      }
+      expect(testableCode.items.length).toEqual(4);
+    });
+  
+    it('expects "Loaded to Empty: using pop 4 times (0 objects)"', function() {
+      testableCode.items = [1,2,3,4];
+      var pop = 4;
+      for (var i = 0, len = pop; i < len; i++) {
+        testableCode.pop();
+      }
+      expect(testableCode.items.length).toEqual(0);
+    });
+  
+    it('expects "Loaded to Empty: using clear 1 time (0 objects)"', function() {
+      testableCode.items = [1,2,3,4];
+      testableCode.clear();
+      expect(testableCode.items.length).toEqual(0);
+    });
+  
+    it('expects "Loaded to Full: using push 6 times (10 objects)"', function() {
+      testableCode.items = [1,2,3,4];
+      var push = 6;
+      for (var i = 0, len = push; i < len; i++) {
+        testableCode.push(i);
+      }
+      expect(testableCode.items.length).toEqual(10);
+    });  
   });
 
   describe('EXCEPTIONS ...', function() {
